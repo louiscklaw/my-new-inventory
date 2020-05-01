f0d96f1d6b4a137bf80c73f394ed4dc4869b0ffa
louiscklaw <louiscklaw@gmail.com>
2020-05-01 19:54:19 +0800

    update and test,

--- publish.sh
+++ publish.sh
@@ -2,16 +2,22 @@
 
 set -ex
 
-rm -rf ../gh-pages/*
+# sync submodule
+git submodule update --init --recursive themes/ace-documentation
+
+# regen
+mkdir -p ../gh-pages
+git worktree add ../gh-pages gh-pages
 
 hugo --minify --enableGitInfo --ignoreCache
 
+# deploy
 cd ../gh-pages
-
-git add .
-git commit -m"update github pages,"
-git push
-
+  git add .
+  git commit -m "$1"
+  git push
 cd -
+
+rm -rf ../gh-pages
 
 echo 'done'
